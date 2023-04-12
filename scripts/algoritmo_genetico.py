416e0228fa8ab3a5e8802e4a8988d11498c95b91
joaoppagnan <joaopedroopagnan@gmail.com>
2023-04-12 17:20:49 -0300

    mudei o algoritmo de uma classe para um método

--- scripts/algoritmo_genetico.py
+++ scripts/algoritmo_genetico.py
@@ -5,34 +5,25 @@ from calc_fitness_QAP import calc_fitness_QAP
 from selecao_torneio import selecao_torneio
 
 
-class AlgoritmoGeneticoQAP(object):
-    def __init__(self, N_populacao: int, n_geracoes: int, q_torneio: float, criterio_de_parada: float,
-                 path_mat_dados: str = 'dados/elshafei_QAP.mat'):
-        """
-        Construtor do algoritmo genetico
-        :param int N_populacao: número de indivíduos na população
-        :param int n_geracoes: quantidade de geracoes
-        :param float q_torneio: fração da população presente no torneio
-        :param float criterio_de_parada: critério de parada a ser utilizado
-        :param str path_mat_dados: arquivo que os dados estão armazenados
-        """
-        mat_dados = scipy.io.loadmat(path_mat_dados)
-        self.distancias = mat_dados['D']
-        self.fluxos = mat_dados['W']
-        self.N_populacao = N_populacao
-        self.n_geracoes = n_geracoes
-        self.q_torneio = q_torneio
-        self.criterio_de_parada = criterio_de_parada
+def algoritmo_genetico(n_populacao: int, n_geracoes: int, q_torneio: float, criterio_de_parada: float,
+                       path_mat_dados: str = 'dados/elshafei_QAP.mat'):
+    """
+    Funcao principal do algoritmo genetico
+    :param n_populacao: número de indivíduos na população
+    :param n_geracoes: quantidade de geracoes
+    :param q_torneio: fração da população presente no torneio
+    :param criterio_de_parada: critério de parada a ser utilizado
+    :param str path_mat_dados: caminho para a matriz com os dados
+    :return: melhor individuo e melhor fitness
+    """
 
-    def algoritmo_genetico(N_populacao, n_geracoes, q_torneio, criterio_de_parada):
-        """
-        Funcao principal do algoritmo genetico
-        :param N_populacao: número de indivíduos na população
-        :param n_geracoes: quantidade de geracoes
-        :param q_torneio: fração da população presente no torneio
-        :param criterio_de_parada: critério de parada a ser utilizado
-        :return: melhor individuo e melhor fitness
-        """
+    # carrega os dados
+    mat_dados = scipy.io.loadmat(path_mat_dados)
+    distancias = mat_dados['D']
+    fluxos = mat_dados['W']
 
-        # gerar população inicial
-        populacao_ini = gerar_pop(N_populacao, )
+    # vê quantos alelos serão necessários
+    n_alelos = fluxos.shape[0]
+
+    # gerar população inicial
+    populacao_ini = gerar_pop(n_populacao, n_alelos)
--- scripts/gerar_pop.py
+++ scripts/gerar_pop.py
@@ -1,20 +1,20 @@
 import numpy as np
 
 
-def gerar_pop(N, n):
+def gerar_pop(n_populacao, n_alelos):
     """
     Gera a população inicial com base em uma permutação aleatória
-    :param N: tamanho da população inicial
-    :param n: quantidade de alelos
+    :param n_populacao: tamanho da população inicial
+    :param n_alelos: quantidade de alelos
     :return: população inicial num array do numpy
     """
     populacao = []
-    for i in range(0, N):
+    for i in range(0, n_populacao):
         individuo = []
-        for j in range(0, n):
-            gene = np.random.randint(low=0, high=n)
+        for j in range(0, n_alelos):
+            gene = np.random.randint(low=0, high=n_alelos)
             while gene in individuo:
-                gene = np.random.randint(low=0, high=n)
+                gene = np.random.randint(low=0, high=n_alelos)
             individuo.append(gene)
         populacao.append(individuo)
     return np.array(populacao)
