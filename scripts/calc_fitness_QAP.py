84c3d59df47a17f0a37bc1d74e27847c72e53b24
joaoppagnan <joaopedroopagnan@gmail.com>
2023-04-11 23:34:19 -0300

    Terminei de implementar a função q calcula o fitness

--- scripts/calc_fitness_QAP.py
+++ scripts/calc_fitness_QAP.py
@@ -2,22 +2,30 @@ import numpy as np
 import scipy.io
 
 
-def calc_fitness_QAP(individuo, n_alelos, distancias, fluxos):
+def calc_fitness_QAP(populacao, n_alelos, distancias, fluxos):
     """
     Calcula o fitness para um problema do tipo QAP
-    :param individuo: individuo que terá o fitness calculado
+    :param populacao: populacao que terá o fitness calculado
     :param n_alelos: quantidade de alelos no código genético da população
     :param distancias: distância entre as localidades
     :param fluxos: fluxo entre as instalações
     :return: um array com os fitness
     """
-    fitness = 0
-    casos_vistos = []
-    for i in range(0, n_alelos):
-        for j in range(0, n_alelos):
+    fitness_pop = []
+    for k in range(0, len(populacao)):
+        try:
+            individuo = populacao[k, 0]
+        except:
+            individuo = populacao[k]
+        fitness = 0
+        casos_vistos = []
+        for i in range(0, n_alelos):
+            for j in range(0, n_alelos):
 
-            # para evitar casos repetidos
-            if (i, j) in casos_vistos or (j, i) in casos_vistos: continue
+                # para evitar casos repetidos
+                if (i, j) in casos_vistos or (j, i) in casos_vistos: continue
 
-            fitness += distancias[i, j]*fluxos
-
+                fitness += distancias[i, j]*fluxos[individuo[i], individuo[j]]
+                casos_vistos.append((i, j))
+        fitness_pop.append(fitness)
+    return np.array(fitness_pop)
